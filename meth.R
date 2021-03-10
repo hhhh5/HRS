55118b5567e06435ec8dca4f8b00433d970640ac
hhhh5 <hhhh5@posteo.de>
2021-03-09 22:44:34 -0600

    Mark and drop suspicious samples

--- meth.R
+++ meth.R
@@ -8,19 +8,19 @@ pheno = pheno[,.(
 	 FID
 	,plate = Sample_Plate # {Thyagarajan_Sample_127}
 	,file = paste0('/nfs/turbo/bakulski1/Datasets/HRS/jonheiss/sensitive/idats/',Slide,'_',Array)
-	,reported_sex = Reported
+	,sex = ifelse(Reported == 'M','m','f')
 	,age = PAGE  # patient age  
 	,BIRTHMO     # ‘%Y’ Year with century.
 	,BIRTHYR     # ‘%m’ Month as decimal number (01-12).
 	,COLLECTDATE # {%Y-%m-%d %H:%M:%S}
 	)]
 
-## --------
+## ---------------------------------------------------
 ## Are all .idat files available?
 table(file.exists(pheno$file %s+% '_Grn.idat'))
 table(file.exists(pheno$file %s+% '_Red.idat'))
 
-## --------
+##---------------------------------------------------
 ## Check the age variable for consistency with collection date/birthday
 
 pheno$COLLECTDATE %<>%
@@ -38,8 +38,8 @@ pheno[abs(age-calculated_age) > 2]
 
 pheno[,c('calculated_age','BIRTHMO','BIRTHYR','COLLECTDATE','birthday'):=NULL]
 
-## --------
-# Read in .idat files in chunks
+## ---------------------------------------------------
+## Read in .idat files in chunks
 
 common = intersect(
 	 ewastools:::manifest_450K[probe_type!="rs"]$probe_id
@@ -49,16 +49,13 @@ common = intersect(
 snps = ewastools:::manifest_epic[probe_type=="rs"]$probe_id
 autosomal = ! ewastools:::manifest_epic$chr %in% c('chrX','chrY')
 
-# Store matrix of beta-values on hard drive
-meth = ff(initdata=NA_real_,dim =c(length(common),pheno[,.N]))
+meth = matrix(NA_real_,nrow=length(common),ncol=pheno[,.N])
 rownames(meth) = common
 
 pheno[,j:=.I]
 chunks = split(pheno,pheno$j %/% 200)
 
 f = function(pheno){
-
-	cat('sdf')
 
 	tmp = 
 		pheno$file %>%
@@ -101,3 +98,84 @@ snps    = chunks %>% map('snps')    %>% do.call('cbind',.)
 metrics = chunks %>% map('metrics') %>% rbindlist
 pheno   = chunks %>% map('pheno')   %>% rbindlist
 
+## ---------------------------------------------------
+## Quality control
+
+pheno[,failed:=FALSE]
+
+
+## -----------
+## Sex check
+pheno[,predicted_sex:=predict_sex(X,Y,which(sex=="m"),which(sex=="f"))]
+table(pheno$sex == pheno$predicted_sex)
+# FALSE  TRUE 
+#     1  3994 
+
+png('intermediate/qc1.png')
+tmp = pheno[sex==predicted_sex]
+plot  (Y ~ X,data=tmp,pch=ifelse(tmp$sex=="f",1,4),asp=1,xlab="Normalized X chromosome intensities",ylab="Normalized Y chromosome intensities")
+tmp = pheno[sex!=predicted_sex]
+points(Y ~ X,data=tmp,pch=ifelse(tmp$sex=="f",1,4),col=2)
+legend("topright",pch=c(1,4),legend=c("female","male"))
+dev.off()
+
+pheno[sex!=predicted_sex,failed:=TRUE]
+
+## -----------
+## Undetected probes
+table(pheno$undetected > 1e5)
+# FALSE  TRUE 
+#  3534   484
+pheno[undetected > 1e5,failed:=TRUE]
+
+## -----------
+## SNP outliers
+tmp = call_genotypes(snps)
+pheno$snp_outlier = snp_outliers(tmp)
+table(pheno$snp_outlier > -3)
+# FALSE  TRUE 
+#  3910   108 
+
+pheno[snp_outlier > -3,failed:=TRUE]
+
+png("intermediate/snps.png")
+stripchart(pheno$snp_outlier,m="j")
+abline(v=-3,lty=3,col=2)
+dev.off()
+
+
+## -----------
+## Control probe metrics
+i = sample_failure(metrics)
+table(i)
+# FALSE  TRUE 
+#  3836   182 
+pheno[i,failed:=TRUE]
+
+
+tmp = copy(metrics)
+tmp$plate = pheno$plate
+tmp = melt(tmp,id.var='plate')
+
+p = ( ggplot(tmp)
++ geom_boxplot(aes(x=plate,y=value))
++ facet_wrap(facets=~variable,ncol=2,scales="free_y")
+)
+
+ggsave(p,file="intermediate/metrics.pdf")
+
+## ---------------------------------------------------
+
+keep = which(pheno$failed==FALSE)
+
+pheno   = pheno  [ keep]
+meth    = meth   [,keep]
+snps    = snps   [,keep]
+metrics = metrics[ keep]
+
+save(pheno,snps,metrics,autosomal,common,file='intermediate/processed.rda')
+
+# Store matrix of beta-values on hard drive
+meth = as.ff(meth,filename='intermediate/meth.ff')
+
+rm(tmp,f,i,p,chunks,keep)
