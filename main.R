aed7290756d2fb2277735490f341c18ad79243f2
hhhh5 <hhhh5@posteo.de>
2021-03-28 22:29:30 -0500

    Reorganize

---
+++ main.R
@@ -0,0 +1,50 @@
+library(haven)
+library(readxl)
+library(data.table)
+library(magrittr)
+library(stringi)
+library(ewastools)
+library(ppcor)
+library(parallel)
+library(svd)
+library(purrr)
+library(robustbase)
+library(quadprog)
+library(ggplot2)
+library(ff)
+
+
+source('cytometry.R')
+source('meth.R')
+
+ffload('intermediate/meth')
+  load('intermediate/meth.RData')
+  load('intermediate/processed.rda')
+
+# ---------------------------------------------------
+
+pheno = LC[pheno,on='FID']
+
+# ---------------------------------------------------
+# Split in test/train
+
+plates = as.character(98:129)
+plates = stri_pad(plates,width=3,pad='0')
+plates = paste0('Thyagarajan_Sample_',plates)
+
+train = pheno[plate %in% plates]$j
+test  = setdiff(pheno$j,train)
+
+cat('# samples in training set:',length(train),'\n')
+cat('# samples in test     set:',length(test ),'\n')
+
+rm(plates)
+
+# ---------------------------------------------------
+# Model training
+
+source('train.R')
+write.table(coefs,file='intermediate/HRS.txt')
+
+# ---------------------------------------------------
+# Model evaluation
--- train.R
+++ train.R
@@ -1,48 +1,3 @@
-library(haven)
-library(readxl)
-library(data.table)
-library(magrittr)
-library(stringi)
-library(ewastools)
-library(ppcor)
-library(parallel)
-library(svd)
-library(purrr)
-library(robustbase)
-library(quadprog)
-library(ggplot2)
-library(ff)
-
-
-source('cytometry.R')
-source('meth.R')
-
-ffload('intermediate/meth')
-  load('intermediate/meth.RData')
-  load('intermediate/processed.rda')
-
-# ---------------------------------------------------
-
-pheno = LC[pheno,on='FID']
-
-# ---------------------------------------------------
-# Split in test/train
-
-plates = as.character(98:129)
-plates = stri_pad(plates,width=3,pad='0')
-plates = paste0('Thyagarajan_Sample_',plates)
-
-train = pheno[plate %in% plates]$j
-test  = setdiff(pheno$j,train)
-
-cat('# samples in training set:',length(train),'\n')
-cat('# samples in test     set:',length(test ),'\n')
-
-rm(plates)
-
-# ---------------------------------------------------
-# Model training
-
 cat('Model training\n')
 
 # do not include NE among the covariates
@@ -79,5 +34,3 @@ coefs = t(coefs)
 coefs[,-1] = coefs[,-1]+coefs[,1]
 colnames(coefs)[1] = 'NE'
 rownames(coefs) = markers
-
-write.table(coefs,file='intermediate/HRS.txt')
